c2da84580fab3f8d71fc0d4174e05f0e06918668
alexandrtovmach <alexandrtovmach@gmail.com>
2017-05-30 00:27:04 +0300

    update

--- general.js
+++ general.js
@@ -1,38 +1,43 @@
+var phaseC = true;
+var start = new Date(2017, 1);
+var end = new Date(2018, 0);
+
 function bg() {
 	if (navigator.platform != "Win32") {
 		document.getElementById("wrapper").style.display = 'none';
-		document.body.style.background = 'url("bg(mob).png")';
+		document.body.style.background = 'url("bgmob.png")';
 	}
 	setInterval(function () {timer()}, 1000)
 }
 
+
 function timer() {
 	var timert = document.getElementById("timert");
-	//var timer = document.getElementById("timer");
-	var start = new Date(2017, 1);
 	var date = new Date();
-	var lost = (date - start);
-	timert.rows[2].cells[0].innerHTML = (date.getFullYear()-start.getFullYear())
-	timert.rows[2].cells[1].innerHTML = (date.getMonth()-start.getMonth())
-	timert.rows[2].cells[2].innerHTML = Math.floor(lost/86400000)
-	timert.rows[2].cells[3].innerHTML = Math.floor(lost/3600000)
-	timert.rows[2].cells[4].innerHTML = Math.floor(lost/60000)
-	timert.rows[2].cells[5].innerHTML = Math.floor(lost/1000)
-	
-	var timerd = document.getElementById("timerd");
-	timerd.style.display = "none";
-	timerd.rows[2].cells[0].innerHTML = (date.getFullYear()-start.getFullYear())
-	timerd.rows[2].cells[1].innerHTML = (date.getMonth()-start.getMonth())
-	timerd.rows[2].cells[2].innerHTML = Math.floor(lost/86400000)
-	timerd.rows[2].cells[3].innerHTML = Math.floor(lost/3600000)
-	timerd.rows[2].cells[4].innerHTML = Math.floor(lost/60000)
-	timerd.rows[2].cells[5].innerHTML = Math.floor(lost/1000)
+	if (phaseC) {
+		var lost = (date - start);
+		timert.rows[0].cells[0].innerHTML = 'Потерянное время'
+		timert.rows[2].cells[0].innerHTML = Math.ceil(lost/(86400000*30))
+		timert.rows[2].cells[1].innerHTML = Math.ceil(lost/86400000)
+		timert.rows[2].cells[2].innerHTML = Math.ceil(lost/3600000)
+		timert.rows[2].cells[3].innerHTML = Math.ceil(lost/60000)
+		timert.rows[2].cells[4].innerHTML = Math.ceil(lost/1000)
+		timert.rows[2].cells[5].innerHTML = String((lost/(end-start))*100).substr(0, 5) + '%'
+	} else {
+		var too = (end - date);
+		timert.rows[0].cells[0].innerHTML = 'Оставшееся время'
+		timert.rows[2].cells[0].innerHTML = Math.floor(too/(86400000*30))
+		timert.rows[2].cells[1].innerHTML = Math.floor(too/86400000)
+		timert.rows[2].cells[2].innerHTML = Math.floor(too/3600000)
+		timert.rows[2].cells[3].innerHTML = Math.floor(too/60000)
+		timert.rows[2].cells[4].innerHTML = Math.floor(too/1000)
+		timert.rows[2].cells[5].innerHTML = String((too/(end-start))*100).substr(0, 5) + '%'
+	}
 }
 
 
 function phase() {
 	var on = document.getElementById("checker");
 	on.classList.toggle("right");
-	timert.style.display = "none";
-	timerd.style.display = "block";
+	phaseC = !phaseC;
 }
