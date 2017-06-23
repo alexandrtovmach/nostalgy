6788dd2b2b08d2e9f8f11bdf1502592ef2ff7e86
Alexandr Tovmach <alexandrtovmach@gmail.com>
2017-06-23 17:42:21 +0300

    im hungry

--- js/general.js
+++ js/general.js
@@ -4,3 +4,20 @@ function init() {
 	myproj.addEventListener('mouseup', myProjSize);
 	myproj.addEventListener('mouseup', bars);
 }
+function polifylls() {
+	(function (arr) {
+	arr.forEach(function (item) {
+		if (item.hasOwnProperty('remove')) {
+		return;
+		}
+		Object.defineProperty(item, 'remove', {
+		configurable: true,
+		enumerable: true,
+		writable: true,
+		value: function remove() {
+			this.parentNode.removeChild(this);
+		}
+		});
+	});
+	})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+}
